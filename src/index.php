d69a528e6e2d476330bc18dd7ec835eb45ecbddd
fennereduardo <fennereduardo@gmail.com>
2023-09-12 15:59:46 -0500

    :construction: New inteface and new trait

--- src/index.php
+++ src/index.php
@@ -2,8 +2,19 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use App\classes\Animal;
+use App\classes\animals\Gato;
 use App\views\PageRenderer;
 
 PageRenderer::render();
 
 echo '<hr>';
+
+$animal = new Animal();
+$animal->hablar();
+$animal->caminar();
+
+echo '<hr>';
+
+$gato = new Gato();
+$gato->hablar();
---
+++ src/interfaces/NewInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\interfaces;
+
+interface NewInterface
+{
+    public function nadar();
+}
